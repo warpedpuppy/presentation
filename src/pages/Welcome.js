356f4a35ea1b8c757f0bcd905f8305fe03673da8
ted <ted@warpedpuppy.com>
2021-06-16 15:44:14 -0400

    added pages

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -18,9 +18,12 @@ export default function Welcome() {
                 <span></span>
                 <span></span>
             </div>
-            <div className="welcome-text">
+            <div className="welcome-header">
                 <h1>welcome</h1>
                 <h2>there be dragons here!</h2>
+            </div>
+            <div className="welcome-text">
+                <p>first and foremost, what I'd love to communicate, is that the world of web dev is awesome and full of opportunity and beauty.  Also endless head and eye aches, finger strain, </p>
             </div>
         </div>
     )
