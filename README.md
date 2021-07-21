e0619d14b32ddff2b1b191dd447ea99b5a20a365
ted <ted@warpedpuppy.com>
2021-07-21 07:43:53 -0400

    moved content

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # Things I Wish I'd Known When I Started as a Web Developer
 ## Ted Walther Presentation for CareerFoundry
-
--- src/pages/Section5.js
+++ src/pages/Section5.js
@@ -2,6 +2,15 @@ import React from 'react'
 import FishBackground from '../components/FishBackground/FishBackground';
 import './Section.css';
 export default function Section5() {
+
+    function expandContent(){
+        let et = document.getElementById('extra-time')
+        if (et.classList.contains('expanded')) {
+            et.classList.remove('expanded')
+        } else {
+            et.classList.add('expanded')
+        }
+    }
     return (
         <div className="section-shell">
              <FishBackground />
@@ -12,23 +21,8 @@ export default function Section5() {
                 <li>tack % onto estimates. . . </li>
                 <li>consider doing css first for client happiness. . . </li>
                 <li>don't underbill in an attempt to impress clients -- you will have set up false expectations for future work.</li>
-                <li>consider making a console log function that is dependent on a boolean. . . </li>
                 <li>never do not include steps for replication when asking for help. . . </li>
                 <li>when working in a huge application. . . .</li>
-                <li>
-                    really common coding errors:
-                    <ul>
-                        <li>Keep your eye on mutations. . . . </li>
-                        <li>Keep your eye on "this". . . . </li>
-                    </ul>
-                </li>
-                <li>
-                    sometimes code can behave unexpectedly:
-                    <ul>
-                        <li>When is 'not a number' not 'not a number'?</li>
-                        <li>Make sure you understand the shorthand you're using. . . . </li>
-                    </ul>
-                </li>
                 <li>Reverse engineering is fair game!</li>
                 <li>Honor your artists!</li>
                 <li>When completely stuck. . . . </li>
@@ -37,8 +31,27 @@ export default function Section5() {
                 <li>Fear not the logs or the error message. . .</li>
                 <li>Identify guesses as guesses. . .</li>
                 <li>"I don't know" is trait of maturity. . . </li>
-                <li>Remember OG metatags</li>
                 <li>Ghosts in the machine. . . </li>
+                <li><span className="extra-time-trigger" onClick={ expandContent }>Extra time?</span>
+                    <ul id="extra-time">
+                        <li>consider making a console log function that is dependent on a boolean. . . </li>
+                        <li>Remember OG metatags</li>
+                        <li>
+                            really common coding errors:
+                            <ul>
+                                <li>Keep your eye on mutations. . . . </li>
+                                <li>Keep your eye on "this". . . . </li>
+                            </ul>
+                        </li>
+                        <li>
+                            sometimes code can behave unexpectedly:
+                            <ul>
+                                <li>When is 'not a number' not 'not a number'?</li>
+                                <li>Make sure you understand the shorthand you're using. . . . </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
             </ul>
             </div>
         </div>
